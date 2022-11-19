f54d5d369a004dba66ecccf8e54bda264d8cfc4b
Kirill Bulatov <Daffy_duck98@yahoo.com>
2022-11-19 23:16:53 +0300

    codeWars

---
+++ codeWars.js
@@ -0,0 +1,33 @@
+// function unscrambleEggs(word) {
+//   return word.replace(/(egg)/gi, "");
+// }
+// console.log(unscrambleEggs("Beggegeggineggnegger"));
+
+// function solution(string) {
+//   let strNew = "";
+
+//   for (let i = 0; i < string.length; i++) {
+//     if (string[i] === string[i].toUpperCase()) {
+//       strNew += " " + string[i];
+//     } else {
+//       strNew += string[i];
+//     }
+//   }
+
+//   return strNew;
+// }
+
+// console.log(solution("camelCasing"));
+
+function nbYear(p0, percent, aug, p) {
+  let result = 0;
+  while (p0 <= p) {
+    p0 = Math.floor(p0 + p0 * (percent / 100) + aug);
+    result++;
+  }
+  console.log("sum", p0);
+  console.log("count", result);
+  return result;
+}
+
+console.log(nbYear(1500, 5, 100, 5000));
